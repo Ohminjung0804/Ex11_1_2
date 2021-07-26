75dbff2f3ee13eee8bc48a2f8954306dffb8afb1
OHMINJUNG <w2009@e-mirim.hs.kr>
2021-07-26 11:48:44 +0900

    Edit MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2009/ex11_1_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2009/ex11_1_2/MainActivity.java
@@ -3,12 +3,27 @@ package kr.hs.emirim.w2009.ex11_1_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] arr = {"아이유","태연","윈터","백현","태민","재현","디오"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list1 = findViewById(R.id.list1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+        list1.setAdapter(adapter);
+
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), arr[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
